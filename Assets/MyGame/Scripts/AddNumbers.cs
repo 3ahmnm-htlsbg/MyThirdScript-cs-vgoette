c81588d4888548407f07f90765e073ed6bddeceb
GoetteVictoria <victoria.goette01@htl-salzburg.ac.at>
2019-11-09 14:23:04 +0100

    add code, clean code, remove code not used

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class AddNumbers : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+        int a = 3;
+        int b = 4;
+
+        Debug.Log ("a = " + a);
+        Debug.Log ("b = " + b);
+        Debug.Log ("a + b = " + (a + b));
+
+        Debug.Log ("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
